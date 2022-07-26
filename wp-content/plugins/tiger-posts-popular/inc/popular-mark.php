7e355f8b2fcde59f919012edf0c7c4ad00adb793
Hai Huynh <hai.huynh@kyanon.digital>
2022-07-26 15:14:40 +0700

    update plugin tiger post popular

--- wp-content/plugins/tiger-posts-popular/inc/popular-mark.php
+++ wp-content/plugins/tiger-posts-popular/inc/popular-mark.php
@@ -2,9 +2,10 @@
 
 // add custom column title for custom meta value
 // 'manage_pages_columns' or 'manage_edit-post_columns' both works
-add_filter('manage_posts_columns', 'ws365150_add_custom_columns_title_pt', 10, 2 );
-function ws365150_add_custom_columns_title_pt( $columns, $post_type ) {
-	switch ( $post_type ) {
+add_filter('manage_posts_columns', 'ws365150_add_custom_columns_title_pt', 10, 2);
+function ws365150_add_custom_columns_title_pt($columns, $post_type)
+{
+	switch ($post_type) {
 		case 'post':
 		case 'episode':
 			$columns['ws365150_popular'] = 'Popular'; // you may use __() later on for translation support
@@ -19,13 +20,14 @@ function ws365150_add_custom_columns_title_pt( $columns, $post_type ) {
 }
 
 // add custom column data with custom meta value for custom post types
-add_action('manage_posts_custom_column', 'ws365150_add_custom_column_data_pt', 10, 2 );
-function ws365150_add_custom_column_data_pt( $column_name, $post_id ) {
-	switch ( $column_name ) {
+add_action('manage_posts_custom_column', 'ws365150_add_custom_column_data_pt', 10, 2);
+function ws365150_add_custom_column_data_pt($column_name, $post_id)
+{
+	switch ($column_name) {
 		case 'ws365150_popular': // specified for this column assigned in the column title
 
-			if ($index = get_post_meta( $post_id, 'popular', true)) {
-				echo '<i class="fa-solid fa-star" style="color: #ff0000"></i>' .' ' . $index;
+			if ($index = get_post_meta($post_id, 'popular', true)) {
+				echo '<i class="fa-solid fa-star" style="color: #ff0000"></i>' . ' ' . $index;
 			}
 			break;
 
@@ -33,24 +35,27 @@ function ws365150_add_custom_column_data_pt( $column_name, $post_id ) {
 			break;
 	}
 }
+
 // for Post + custom post type
-add_action( 'quick_edit_custom_box', 'ws365150_custom_edit_box_pt', 10, 3 );
-function ws365150_custom_edit_box_pt( $column_name, $post_type, $taxonomy ) {
+add_action('quick_edit_custom_box', 'ws365150_custom_edit_box_pt', 10, 3);
+function ws365150_custom_edit_box_pt($column_name, $post_type, $taxonomy)
+{
 	global $post;
 
-	switch ( $post_type ) {
+	switch ($post_type) {
 		case 'post':
 		case 'episode':
 
-			if( $column_name === 'ws365150_popular' ): // same column title as defined in previous step
+			if ($column_name === 'ws365150_popular'): // same column title as defined in previous step
 				?>
-				<?php // echo get_post_meta( $post->ID, 'remark', true ); ?>
 				<fieldset class="inline-edit-col-right" id="#edit-">
 					<div class="inline-edit-col">
 						<label>
 							<label class="alignleft">
 								<span class="checkbox-title">Popular index</span>
 								<input type="number" name="popular" placeholder="Index of popular">
+								<button class="clear-value" id="clear_value"><i class="fa-solid fa-circle-xmark"></i>
+								</button>
 							</label>
 						</label>
 					</div>
@@ -65,10 +70,13 @@ function ws365150_custom_edit_box_pt( $column_name, $post_type, $taxonomy ) {
 	}
 }
 
-add_action( 'save_post', 'ws365150_update_custom_quickedit_box' );
-function ws365150_update_custom_quickedit_box() {
-	if( isset( $_POST ) && isset( $_POST['popular'] ) ) { // where summary is defined in the <input name="summary">
+add_action('save_post', 'ws365150_update_custom_quickedit_box');
+function ws365150_update_custom_quickedit_box()
+{
+	if (isset($_POST) && isset($_POST['popular']) && $_POST['popular'] > 0) { // where summary is defined in the <input name="summary">
 		update_post_meta($_POST['post_ID'], 'popular', $_POST['popular']);
+	} else {
+		delete_post_meta($_POST['post_ID'], 'popular');
 	}
 
 	// for debugging purpose in inspector, not necessary, enable this will break the saving but could see the ajax return
@@ -79,19 +87,18 @@ function ws365150_update_custom_quickedit_box() {
 	return; // finish the function call
 }
 
-add_action( 'admin_enqueue_scripts', function( $page ) {
+add_action('admin_enqueue_scripts', function ($page) {
 
 	// add page checking logic, this is a simple one, you may test post type and so on...
-	if ( 'edit.php' != $page ) {
+	if ('edit.php' != $page) {
 		return;
 	}
 
-	wp_enqueue_script( 'custom-quickedit-box', get_stylesheet_directory_uri() . '/ws365150_custom_quickedit_box.js', array( 'jquery', 'inline-edit-post' ) );
+	wp_enqueue_script('popular-quickedit-box', plugin_dir_url(__DIR__) . '/js/popular-mark.js', array('jquery', 'inline-edit-post'));
 });
 
 
-
-function tg_get_posts_popular($postType = 'post', $order = 'DESC') : array
+function tg_get_posts_popular($postType = 'post', $order = 'DESC'): array
 {
 	$args = array(
 		'post_status' => 'publish',
@@ -105,7 +112,7 @@ function tg_get_posts_popular($postType = 'post', $order = 'DESC') : array
 	$query = new WP_Query($args);
 
 	// check if this condition not true then get all post
-	if ($query->found_posts ==0) {
+	if ($query->found_posts == 0) {
 		$query = tg_get_all_post();
 	}
 	return $query->posts;
@@ -125,3 +132,4 @@ function tg_get_all_post(): WP_Query
 
 	return new WP_Query($args);
 }
+
