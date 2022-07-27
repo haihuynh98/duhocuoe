f2c81563ec174019e965e9a323ed5630c7b1453f
Hai Huynh <hai.huynh@kyanon.digital>
2022-07-27 17:57:46 +0700

    fix functions

--- wp-content/themes/duhocuoe/functions.php
+++ wp-content/themes/duhocuoe/functions.php
@@ -1284,7 +1284,7 @@ function load_css_js()
     wp_enqueue_style('product', get_template_directory_uri() . '/css/_product.css', [], false);
     wp_enqueue_style('style-nice', get_template_directory_uri() . '/css/nicepage.css', [], false);
     wp_enqueue_style('home', get_template_directory_uri() . '/css/home.css', [], false);
-    wp_enqueue_style('common-theme', get_template_directory_uri() . '/css/common.css', [], false);
+    wp_enqueue_style('common-theme', get_template_directory_uri() . '/css/common.min.css', [], false);
     wp_enqueue_style('header', get_template_directory_uri() . '/css/header.css', [], false);
     wp_enqueue_style('footer', get_template_directory_uri() . '/css/footer.css', [], false);
     if (is_singular('production')) {
