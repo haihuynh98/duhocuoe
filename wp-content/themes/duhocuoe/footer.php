39fa1739f3e1afffec25d1a66799343e025ff951
Hai Huynh <hai.huynh@kyanon.digital>
2022-09-04 18:44:21 +0700

    add google map to footer

--- wp-content/themes/duhocuoe/footer.php
+++ wp-content/themes/duhocuoe/footer.php
@@ -76,6 +76,13 @@ if (!$hideFooter) { ?>
             </div>
         </div>
     </section>
+    <section class="gmaps" id="gmaps">
+        <div id="map-container-google-1" class="z-depth-1-half map-container container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.552150662682!2d106.68097661744385!3d10.76895730000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f2211e0d953%3A0x216003aa4aeb358!2zRHUgSOG7jWMgVU9F!5e0!3m2!1svi!2s!4v1662291260921!5m2!1svi!2s"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
     <section class="address-footer" id="address_footer">
         <div class="container">
             <div class="row">
