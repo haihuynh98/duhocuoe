ce41f21101097a795142ff96b58b6149e16bddc3
Hai Huynh <hai.huynh@kyanon.digital>
2022-09-04 18:11:23 +0700

    remove nice page plugin

--- wp-content/plugins/nicepage/editor/assets/js/meta-boxes.js
+++
@@ -1,8 +0,0 @@
-//For edit page meta boxes
-jQuery(document).ready(function($){
-    if (parent.dataBridge) {
-        var protectedWithNp = parent.dataBridge.getInfo().protectedWithNp || false;
-        var type = protectedWithNp ? 'password' : 'text';
-        jQuery('#post_password').attr('type', type);
-    }
-});
--- wp-content/plugins/nicepage/includes/controls/languageLink/languageLink.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('ABSPATH') or die;
-
-if (isset($controlProps) && isset($controlTemplate)) {
-    $language = isset($controlProps['lang']) && $controlProps['lang'] ? $controlProps['lang'] : false;
-    $url = NpMultiLanguages::getLanguageLink($language);
-    $controlTemplate = str_replace('[[url]]', $url, $controlTemplate);
-    echo $controlTemplate;
-}
